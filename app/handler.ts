45eede29dfd9144f091633f83a059ba010421815
Hamza Umar Farooq Ghani <h.u.f.ghani@gmail.com>
2021-03-05 22:28:03 +0000

    now running on rest protcol

--- app/handler.ts
+++ app/handler.ts
@@ -1,7 +1,15 @@
-import { Context, Handler } from 'aws-lambda'
+import { Context } from 'aws-lambda'
 import { GithubController } from './controller/github'
 
 const githubController = new GithubController()
-export const githubProjectRepos: Handler = (event: any, context: Context) => {
-  return githubController.ProjectRepos(event, context)
+export async function githubProjectRepos(event: any, context: Context) {
+  const result = await githubController.ProjectRepos(event, context)
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      code: 0,
+      message: 'success',
+      data: result,
+    }),
+  }
 }
